d854f4fb3518fa78bf6bef7d5c8a2024c9d780c6
OscarEnriqueGR <126024964+OscarEnriqueGR@users.noreply.github.com>
2023-03-01 18:04:24 -0500

    Delete Tipos de triángulos según sus lados.py

--- Tipos de triángulos según sus lados.py
+++
@@ -1,9 +0,0 @@
-A= int (input ("Primer lado del triángulo "))
-B= int (input ("Segundo lado del triángulo "))
-C= int (input ("Terncer lado del triángulo "))
-if A == B and B == C:
-    print ("Equilatero")
-elif (A == B and A != C) or (A == C and A != B) or (B == C and B != A):
-    print ("Isósceles")
-else:
-    print ("Escaleno")
